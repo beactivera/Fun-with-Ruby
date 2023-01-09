a9077b2d77351ef55952d48a97f5220926bc5c54
beactivera <weronika.wdowiak56@gmail.com>
2023-01-09 14:25:45 +0100

    Made a loop with extra functions

--- guessingGame/main.rb
+++ guessingGame/main.rb
@@ -2,14 +2,17 @@ def random_number_generator()
    return rand(1..100) 
 end
 
-def is_number_the_same(machine_number, user_number)
-    if machine_number == user_number
-        puts "Your number is correct!"
-    elsif
-        puts "Your number is incorrect:("
-    end
+# def is_number_the_same(machine_number, user_number)
+#     return machine_number == user_number
+# end
+
+def is_too_big(machine_number, user_number)
+    return machine_number < user_number
 end
 
+def is_too_small(machine_number, user_number)
+    return machine_number > user_number
+end
 
 puts "Hi User! \nWelcome in Guessing Game! \nThe Game Machine is randomizing the number in range 1-100\n
        Would you like to quess the number?(y/n)\n"
@@ -19,11 +22,26 @@ user_answer = gets.chomp
 if user_answer == "y"
     puts "\nSo let's play the game.Good luck!\n\n"
     number_from_the_machine = random_number_generator()
-    puts "#{number_from_the_machine} \n"
-    puts "The machine drew a number.\n\n What number is it?\n"
-    guessed_number = gets.chomp
-    is_number_the_same(number_from_the_machine, guessed_number.to_i)
+    # for testing
+    puts "(---#{number_from_the_machine}---)\n"
 
+    puts "The machine drew a number.\n\n What is the number? "
+
+    guessed_number = gets.chomp
+    user_number = guessed_number.to_i
+    # guess_result = is_number_the_same(number_from_the_machine, user_number)
+
+    while number_from_the_machine != user_number
+        puts "Your number is incorrect :("
+        if is_too_big(number_from_the_machine, user_number)
+            puts "Your guess is too big.\n"
+            user_number = guess_one_more_time()
+        elsif is_too_small(number_from_the_machine, user_number)
+            puts "Your guess is too small.\n"
+            user_number = guess_one_more_time()
+        end
+    end
+    puts "Your guess is correct!"
 elsif user_answer == "n"
     puts "Fine. The program will close soon."  
 else
@@ -33,3 +51,8 @@ end
 puts "\n"
 
 
+def guess_one_more_time()
+    puts "Try again. What is the number? "
+    guessed_number = gets.chomp
+    return guessed_number.to_i
+end
