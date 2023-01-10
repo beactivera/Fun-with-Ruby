e6bcf41b2c0b3c033770012eee87bd8d7af684ae
beactivera <weronika.wdowiak56@gmail.com>
2023-01-10 17:51:02 +0100

    Finding your Strong Number

--- strongNumbers/main.rb
+++ strongNumbers/main.rb
@@ -1,24 +1,35 @@
 puts "Hi User! \nPlease provide you first name: "
- first_name = gets.chomp
- puts "And also please provide you last name: "
- last_name = gets.chomp
+first_name = gets.chomp
+puts "And also please provide you last name: "
+last_name = gets.chomp
 
- nick = first_name[0,3].downcase + last_name[0,3].downcase
- puts "Your nickname is: #{nick}\n\n"
+nick = first_name[0,3].downcase + last_name[0,3].downcase
+puts "Your nickname is: #{nick}\n\n"
 
- bytes = nick.codepoints
- puts "Your ASCI codes are: #{bytes}"
- puts "\n"
+bytes = nick.codepoints
+puts "Your ASCI codes are: #{bytes}"
+puts "\n"
 
- for code in bytes do
-    puts "Factorial for number #{code}:"
-    fact = 1
-    for n in 1..code+1 do
-        fact = fact * n
-        # puts "Factorial function called for number #{n} || result: #{fact}"
+bytes_as_strings = bytes.map(&:to_s)
+puts "#{bytes_as_strings}\n"
+puts "\n"
+
+counter = 0
+fact = 1
+n = 100000
+for i in 1..n+1
+    puts i 
+    fact = fact * i
+    for code in bytes_as_strings do
+        str = fact.to_s
+        if str.include?(code)
+            counter += 1        
+        else
+            counter = 0
+        end
     end
-    puts "   #{fact}"
-    puts "\n\n"
- end
-
-
+    if counter == bytes_as_strings.length()
+        puts "Congratulations, you have founded your Strong Number which is /#{i}!/"
+        break
+    end
+end
