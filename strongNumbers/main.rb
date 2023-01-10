ca09db534f2242fa9e8c9cda44cb14a3a7cae23a
beactivera <weronika.wdowiak56@gmail.com>
2023-01-10 15:20:48 +0100

    Added factorial function with results

--- strongNumbers/main.rb
+++ strongNumbers/main.rb
@@ -1,6 +1,6 @@
-puts "Hi User! \nPlease provide you first name: \n"
+puts "Hi User! \nPlease provide you first name: "
  first_name = gets.chomp
- puts "And also please provide you last name: \n"
+ puts "And also please provide you last name: "
  last_name = gets.chomp
 
  nick = first_name[0,3].downcase + last_name[0,3].downcase
@@ -9,3 +9,16 @@ puts "Hi User! \nPlease provide you first name: \n"
  bytes = nick.codepoints
  puts "Your ASCI codes are: #{bytes}"
  puts "\n"
+
+ for code in bytes do
+    puts "Factorial for number #{code}:"
+    fact = 1
+    for n in 1..code+1 do
+        fact = fact * n
+        # puts "Factorial function called for number #{n} || result: #{fact}"
+    end
+    puts "   #{fact}"
+    puts "\n\n"
+ end
+
+
